37949ef3dc6fa989ca6ced185de1e27f4af6752a
AndIMissU <784738601@qq.com>
2017-12-07 12:33:05 +0800

    完成了本地仓库的显示和增加

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,13 +1,8 @@
-// pages/main/main.js
 const app = getApp()
 Page({
   data: {
     stars:[],
-    newstars:{
-      starname:'',
-      starpic:'',
-      type:''
-    },
+    newstars:[],
     showView:0,
     display:false
   },
@@ -23,21 +18,21 @@ Page({
          type: type
       },
       success: function(res){
-        console.log(res);
        var data = res.data.data[app.data.currentUser];
-        console.log(data);
-      //   console.log(data.name);
         that.setData({
           stars: data.stars
-        })
+        });
+        
       }
-    })
+    });
+
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
   this.getStarsInfo('');
+  
 
   },
 
@@ -53,6 +48,20 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    var that = this;
+    wx.getStorage({
+      key: 'info',
+      success:function(res){
+        that.setData({
+          newstars:res.data
+        });
+      }
+    });
+    if(that.data.newstars.length>0){
+      that.setData({
+        display:true  
+      });
+    }
   
   },
 
@@ -65,54 +74,14 @@ Page({
       key: 'info',
       success:function(res){
         that.setData({
-          newstars:{
-            starname: res.data.starname,
-            starpic: res.data.starpic,
-            type:res.data.type
-          }
+          newstars:res.data
         });
       }
     });
-    console.log(this.data.newstars.starname);
-
-    if(this.data.newstars.starname.length>0){
+    if(this.data.newstars.length>0){
       this.setData({
         display:true  
       });
     }
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
 })
